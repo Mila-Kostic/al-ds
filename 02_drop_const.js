70c71e52b331dca4f009924c320dacf39f73ccdd
Mila Kostic <milakostic.eng@gmail.com>
2022-10-11 07:46:43 +0200

    nesting

--- 02_drop_const.js
+++ 02_drop_const.js
@@ -1,3 +1,4 @@
+// Drop consts - mistake - code run just n + n => O(2n), corect is n * n => 0(n2)=> next example
 function logItems(n) {
   for (let i = 0; i < n; i++) {
     console.log("i" + i);
---
+++ 03_nesting_solve_02.js
@@ -0,0 +1,11 @@
+// Solve drop consts - (mistake - code run just n + n => O(2n)), corect is n * n => 0(n2)=> next example
+
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+
+logItems(10);
