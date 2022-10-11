cfc2cbac4be698852f2cad0de663e4e4b87cf79d
Mila Kostic <milakostic.eng@gmail.com>
2022-10-11 08:10:54 +0200

    sum

---
+++ example/for_loop/01_sum.js
@@ -0,0 +1,13 @@
+// example for theory 01_big_O.js
+
+//Program to display the sum of natural numbers
+let sum = 0;
+const n = 100;
+
+//looping from i = 1 to n
+// in each iteration, i is increased by 1
+for (let i = 1; i <= n; i++) {
+  sum += i; //sum = sum + i
+}
+
+console.log("Sum: ", sum);
