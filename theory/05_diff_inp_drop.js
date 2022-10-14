4e141c6ce6ab21d33e36d063d4142993c7784bc2
Mila Kostic <milakostic.eng@gmail.com>
2022-10-14 07:41:08 +0200

    dif inp drop

---
+++ theory/05_diff_inp_drop.js
@@ -0,0 +1,12 @@
+//Different terms for input
+// O (a + b); => solve in next 06_diff_inp_solve_05.js
+function logItems(a, b) {
+  for (let i = 0; i < a; i++) {
+    console.log(i);
+  }
+  for (let j = 0; j < b; j++) {
+    console.log(j);
+  }
+}
+
+logItems(3, 4);
