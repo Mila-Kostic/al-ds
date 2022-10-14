aa359dd6ee191b0332158ac196ec45061a52e1b8
Mila Kostic <milakostic.eng@gmail.com>
2022-10-14 07:00:44 +0200

    O(n3) => n * n * n

---
+++ theory/04_deepier_nesting.js
@@ -0,0 +1,13 @@
+// O (n3) = n * n * n
+
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < n; k++) {
+        console.log(i, j, k);
+      }
+    }
+  }
+}
+
+logItems(5);
