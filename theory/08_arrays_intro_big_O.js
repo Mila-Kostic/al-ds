d4456374ae398a3f96034f6fe22dfb2467023d7e
Mila Kostic <milakostic.eng@gmail.com>
2022-10-18 08:26:37 +0200

    arrays big-O

---
+++ theory/08_arrays_intro_big_O.js
@@ -0,0 +1,11 @@
+// Big-O of Arrays:
+
+// Insertion — It depends. Insertion at the end is O(1). But insertion at the beginning is o(n)
+// Removal — It depends. Removal at the end is O(1). But in between or at the beginning is o(n)
+// Access — O(1)
+// Searching — O(n)
+// Big-O of Array methods:
+
+// push, pop — O(1)
+// shift, unshift, concat, slice, splice — O(n)
+// forEach/map/filter/reduce/etc. — O(n)
