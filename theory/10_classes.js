45af63c5ce6a297a90858ef16976582ebada82b7
Mila Kostic <milakostic.eng@gmail.com>
2022-10-22 06:45:30 +0200

    classes

---
+++ theory/10_classes.js
@@ -0,0 +1,10 @@
+class Cookie {
+  constructor(color) {
+    this.color = color;
+  }
+}
+
+let cookieOne = new Cookie("green");
+let cookieTwo = new Cookie("blue");
+console.log(cookieOne);
+console.log(cookieTwo);
