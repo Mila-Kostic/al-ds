419f186c7dae6d870ffbb4f917afb48eac1fd5f4
Mila Kostic <milakostic.eng@gmail.com>
2022-11-14 22:36:39 +0100

    promise

---
+++ example/promises/promise.js
@@ -0,0 +1,8 @@
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 2;
+  if (a == 2) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+});
---
+++ theory/17_linked_list_pop.js
@@ -0,0 +1,2 @@
+//1h 15 min tutorial - https://www.youtube.com/watch?v=BVJv_AiXSGg
+
